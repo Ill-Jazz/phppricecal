cdd3e1578b45d793212983540391c51762098d52
Ill-Jazz <iliasguedoura@gmail.com>
2021-06-16 09:18:21 +0200

    adding piece of productloader

--- Model/productsLoader.php
+++ Model/productsLoader.php
@@ -3,5 +3,15 @@
 
 class productsLoader
 {
+    public function getProducts() {
+        $connect = new Connection();
+        $pdo = $connect -> Openconnection();
 
+        $handle = $pdo -> prepare("SELECT * FROM product");
+        $handle -> execute();
+        $products = $handle -> fetchAll();
+
+        $productArray = [];
+        foreach ($products as $product) {
+            $product = new
 }
